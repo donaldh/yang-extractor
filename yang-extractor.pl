42c3682f192bab69a2d8584bedd92f23e85c53cc
Donald Hunter <donaldh@cisco.com>
2016-03-17 15:10:01 +0000

    Strip headers/footers and indentation.

---
+++ yang-extractor.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/env perl6
+
+use v6;
+
+sub MAIN($rfc-file) {
+    my regex begins { '<CODE BEGINS>' }
+    my regex file { 'file' \s+ '"' $<name>=[ <-[ " ]> + ] '"' }
+    my regex body { .*? }
+    my regex ends { '<CODE ENDS>' }
+    my regex block { .*? <begins> \s+ <file> \h* \n+ <body> <ends>  .*? };
+
+    my $text = slurp $rfc-file;
+
+    if $text ~~ / <block>+ / {
+        for $<block>.list -> $block {
+            my $filename = ~$block<file><name>;
+            my $contents =  ~$block<body>;
+
+            # Identify the indent level
+            my $indent = $contents ~~ /^\s+/;
+
+            # Strip page headers and footers
+            $contents ~~ s:g/ \n+ [ ^^ \S \N* ]+ \n+//;
+
+            # Strip leading indentation
+            $contents ~~ s:g/ ^^ "$indent" //;
+
+            say "Saving {+$contents.lines} lines into {$filename}";
+            spurt $filename, $contents;
+        }
+    }
+}
